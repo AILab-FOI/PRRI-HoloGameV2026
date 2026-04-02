b854e798796a911af8f9b8ac24c25c89de1d57c3
vzezelj23 <vzezelj23@student.foi.hr>
2026-04-02 22:30:36 +0200

    Prva verzija prototipa
    - implementirane osnovne kretnje (lijevo, desno, skakanje)
    - implementirana logika kolizije

---
+++ prototip.py
@@ -0,0 +1,131 @@
+# script: python
+
+# --- COLLISION ---
+class Collidable:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+
+    def check(self, other):
+        return self.x < other.x + other.width and self.x + self.width > other.x and self.y < other.y + other.height and self.y + self.height > other.y
+
+
+# --- HELPER ---
+def pomakni(a, b, v):
+    if a < b:
+        return min(a + v, b)
+    else:
+        return max(a - v, b)
+
+
+# --- PLAYER ---
+class Player:
+    def __init__(self):
+        self.x = 50
+        self.y = 50
+        self.width = 14   # <-- BITNO
+        self.height = 14  # <-- BITNO
+
+        self.hsp = 0
+        self.vsp = 0
+
+    def sudar(self, dx, dy, colls):
+        self.x += dx
+        self.y += dy
+
+        for c in colls:
+            if c.check(self):
+                self.x -= dx
+                self.y -= dy
+                return True
+
+        self.x -= dx
+        self.y -= dy
+        return False
+
+    def update(self, colls):
+        # LEFT / RIGHT
+        if key(1):
+            self.hsp = pomakni(self.hsp, -2, 0.3)
+        elif key(4):
+            self.hsp = pomakni(self.hsp, 2, 0.3)
+        else:
+            self.hsp = pomakni(self.hsp, 0, 0.3)
+
+        # JUMP space=48
+        if key(23) and self.sudar(0, 1, colls):
+            self.vsp = -4
+
+        # GRAVITY
+        if not self.sudar(0, self.vsp + 1, colls):
+            self.vsp += 0.25
+        else:
+            self.vsp = 0
+
+        # COLLISION X
+        if self.sudar(self.hsp, 0, colls):
+            self.hsp = 0
+
+        # COLLISION Y
+        if self.sudar(0, self.vsp, colls):
+            self.vsp = 0
+
+        # MOVE
+        self.x += self.hsp
+        self.y += self.vsp
+
+    def draw(self):
+        rect(int(self.x), int(self.y), int(self.width), int(self.height), 12)
+
+
+# --- INIT ---
+player = Player()
+
+colls = [
+    Collidable(0, 120, 240, 16),   # floor
+    Collidable(80, 90, 40, 10),    # platform
+    Collidable(140, 70, 40, 10)    # platform 2
+]
+
+
+# --- MAIN LOOP ---
+def TIC():
+    cls(0)
+
+    player.update(colls)
+    player.draw()
+
+    # debug draw kolizija
+    for c in colls:
+        rect(int(c.x), int(c.y), int(c.width), int(c.height), 1)
+# <TILES>
+# 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+# 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+# 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+# 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+# 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+# 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+# 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+# 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+# </TILES>
+
+# <WAVES>
+# 000:00000000ffffffff00000000ffffffff
+# 001:0123456789abcdeffedcba9876543210
+# 002:0123456789abcdef0123456789abcdef
+# </WAVES>
+
+# <SFX>
+# 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+# </SFX>
+
+# <TRACKS>
+# 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+# </TRACKS>
+
+# <PALETTE>
+# 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+# </PALETTE>
+

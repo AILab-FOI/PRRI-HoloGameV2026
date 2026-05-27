e78853f1ab37963f88415625acb9c94428e9db21
dplodinec22 <162130690+dplodinec22@users.noreply.github.com>
2026-05-27 13:57:27 +0200

    Update ProtocolX.py

--- ProtocolX.py
+++ ProtocolX.py
@@ -1998,6 +1998,13 @@ def draw_menu():
     print("PROTOCOL X", 82, 35, 12)
     print("A CYBERPUNK PLATFORMER", 58, 50, 4)
 
+    if time() // 500 % 2 == 0:
+        print("PRESS W TO START", 70, 80, 11)
+
+    print("ARROWS - MOVE", 76, 100, 13)
+    print("W - JUMP", 94, 110, 13)
+    print("B - ATTACK", 88, 120, 13)
+
 def give_key(key_id):
     if key_id not in player.keys:
         player.keys.append(key_id)
